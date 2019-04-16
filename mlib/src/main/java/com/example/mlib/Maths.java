a005b37c9270a9ec61c071983eafd7b801b772ef
shashankkasera98@gmail.com <shashankkasera98@gmail.com>
2019-04-16 12:32:22 +0530

    Initial commit

---
+++ mlib/src/main/java/com/example/mlib/Maths.java
@@ -0,0 +1,17 @@
+package com.example.mlib;
+
+public class Maths {
+
+
+    public static int sum(int a, int b){
+
+        return a+b;
+    }
+
+    public static int minuse(int a, int b){
+
+        return a-b;
+    }
+
+
+}
